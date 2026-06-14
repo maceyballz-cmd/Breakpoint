906ce5fc1c4c3e8613aa32075881207ae0eadfad
maceyballz-cmd <maceyballz@gmail.com>
2026-06-14 21:52:51 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,20 @@
-const CACHE_NAME = 'workout-v1';
+const CACHE_NAME = 'bp-v1';
 const ASSETS = [
-  'index.html',
-  'style.css',
-  'script.js',
-  'manifest.json'
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json'
 ];
 
-// Install the Service Worker
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-// Fetch assets from cache if offline
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
